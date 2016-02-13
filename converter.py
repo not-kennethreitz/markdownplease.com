e240f4dcf2eac4fad9f23969ce9f8c8a6297c02c
Kenneth Reitz <me@kennethreitz.org>
2016-02-13 10:28:20 -0500

    Revert

--- converter.py
+++ converter.py
@@ -1,44 +1,36 @@
 # -*- coding: utf-8 -*-
 
+
+
+
 import os
 
 import requests
 from html2text import html2text
 
+READABILITY_URL = 'https://www.readability.com/api/content/v1/parser'
 
-_READABILITY_URL = 'https://www.readability.com/api/content/v1/parser'
-
-
-def _get_readability_html_and_title(url):
+def readability(url):
     token = os.environ.get('READABILITY_TOKEN')
     params = {'url': url, 'token': token}
 
-    r = requests.get(_READABILITY_URL, params=params)
-    decoded_content = (
-        r.json()['content'],
-        r.json()['title'],
-    )
-    return decoded_content
+    r = requests.get(READABILITY_URL, params=params)
+    return r.json()['content'], r.json()['title']
 
-
-def _convert_html_to_markdown(html, title=None):
+def convert(html, title=None):
     if title:
         title = '# {}'.format(title)
         html = '\n\n'.join([title, html])
 
-    text_from_html = html2text(html)
-    return text_from_html
+    return html2text(html)
 
-
-def get_readable_content_from_url(url):
+def meh(url):
     try:
-        content, title = _get_readability_html_and_title(url)
-        markdown = _convert_html_to_markdown(content, title=title)
-        return markdown
+        content, title = readability(url)
+        return convert(content, title=title)
     except KeyError:
         return None
 
 
 if __name__ == '__main__':
-    print get_readable_content_from_url('http://kennethreitz.org/')
-
+    print meh('http://kennethreitz.org/')
--- service.py
+++ service.py
@@ -1,40 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, render_template, Markup
-from converter import get_readable_content_from_url
-from markdown import markdown
-
+from flask import Flask, request, redirect, url_for, render_template
+from converter import meh
 
 app = Flask(__name__)
-
-
-def _markdown_to_html(text):
-    return Markup(markdown(text))
-
 
 @app.route('/')
 def fuck_gpl3():
     url = request.args.get('url')
-    type = request.args.get('type', 'markdown')
-
-    content = get_readable_content_from_url(url)
 
     if url:
-        if not content:
-            return '404 Not Found', 404
-
-        if type == 'html':
-            markdown_url_contents = _markdown_to_html(content)
-            return render_template(
-                'index.html',
-                converted_url_contents=markdown_url_contents,
-                page_url=url,
-            )
-        else:
+        content = meh(url)
+        if content:
             return content, 200, {'Content-Type': 'text/x-markdown; charset=UTF-8'}
+        else:
+            return '404 Not Found', 404
     else:
         return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run()
